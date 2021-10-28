5ac6ca4f33cefd1fb8ab2b006e6a268df936d516
Jessika Risberg <33091640+JessikaRisberg@users.noreply.github.com>
2021-10-28 18:22:29 +0200

    set up of panel in gameframe

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,20 @@
-public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+        public GameFrame(){
+            GamePanel panel = new GamePanel();
+
+            // set up
+            this.add(panel);
+            this.setTitle("Snake");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.pack();
+            this.setVisible(true);
+            // Puts the frame in the middle of the screen
+            this.setLocationRelativeTo(null);
+        }
+
+
 }
