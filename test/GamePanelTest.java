2c81e30fee1969bcb16f57019c3bdfdcf847ef47
Jessika Risberg <33091640+JessikaRisberg@users.noreply.github.com>
2021-10-31 19:58:12 +0100

    created test for checkapple method

--- test/GamePanelTest.java
+++ test/GamePanelTest.java
@@ -29,20 +29,36 @@ class GamePanelTest {
     void checkCollision() {
         // set up
         final int screenWidth = 600;
-        final int x[] = new int[];
+        final int x[] = new int[604];
         boolean running = true;
-        //
+
         // checks if snake collied with right border
-        if (x[0] > screenWidth){
+        if (x[0] < screenWidth){
             running = false;
         }
-
+        // Result
         assertFalse(running);
-
     }
 
     @Test
     void checkApple() {
+        // set up
+        final int x[] = new int[25];
+        final int y[] = new int[25];
+        int bodyParts = 10;
+        int applesEaten = 0;
+        int appleX = 0;
+        int appleY = 0;
+
+        // condition
+        if((x[0] == appleX) && (y[0] == appleY)){
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+        // result
+        assertEquals(bodyParts, 11);
+        assertEquals(applesEaten, 1);
     }
 
     @Test
